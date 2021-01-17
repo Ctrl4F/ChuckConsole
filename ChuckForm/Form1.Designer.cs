d405c144037a55549c0bbcea6ffa1bce6c6ed811
Your Name <you@example.com>
2021-01-17 14:08:56 -0800

    first commit

---
+++ ChuckForm/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace ChuckForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.GetJokeBtn = new System.Windows.Forms.Button();
+            this.jokeBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // GetJokeBtn
+            // 
+            this.GetJokeBtn.Location = new System.Drawing.Point(0, 0);
+            this.GetJokeBtn.Name = "GetJokeBtn";
+            this.GetJokeBtn.Size = new System.Drawing.Size(75, 23);
+            this.GetJokeBtn.TabIndex = 0;
+            this.GetJokeBtn.Text = "GetJoke";
+            this.GetJokeBtn.UseVisualStyleBackColor = true;
+            this.GetJokeBtn.Click += new System.EventHandler(this.GetJokeBtn_Click);
+            // 
+            // jokeBox
+            // 
+            this.jokeBox.Location = new System.Drawing.Point(0, 43);
+            this.jokeBox.Name = "jokeBox";
+            this.jokeBox.Size = new System.Drawing.Size(280, 298);
+            this.jokeBox.TabIndex = 1;
+            this.jokeBox.Text = "Get a joke, I dare you!";
+            this.jokeBox.TextChanged += new System.EventHandler(this.jokeBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(749, 369);
+            this.Controls.Add(this.jokeBox);
+            this.Controls.Add(this.GetJokeBtn);
+            this.Name = "Form1";
+            this.Text = "Chuck Norris Jokes Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button GetJokeBtn;
+        private System.Windows.Forms.RichTextBox jokeBox;
+    }
+}
+
